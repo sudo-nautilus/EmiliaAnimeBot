3cf212c5ace6b4d46bc64f9573c32e87cb1cccc0
Cypher Izumi <85168398+IzumiCypherX@users.noreply.github.com>
2021-09-08 11:46:32 +0530

    Update pastebin.py

--- EmiliaAnimeBot/utils/pastebin.py
+++ EmiliaAnimeBot/utils/pastebin.py
@@ -1,1 +1,10 @@
+from wbb.utils.http import post
 
+BASE = "https://batbin.me/"
+
+
+async def paste(content: str):
+    resp = await post(f"{BASE}api/paste", data={"content": content})
+    if not resp["status"]:
+        return
+    return BASE + resp["message"]
